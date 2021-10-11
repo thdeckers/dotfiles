16215b1afbff16e76422e16dc325b36a4d01521d
Thomas Deckers <thomas@deckers.one>
2021-10-11 17:44:49 +0200

    Only for spotify

--- private_dot_config/waybar/modules/executable_mediaplayer.sh
+++ private_dot_config/waybar/modules/executable_mediaplayer.sh
@@ -2,9 +2,9 @@
 # copied from https://github.com/Egosummiki/dotfiles/blob/master/waybar/mediaplayer.sh
 player_status=$(playerctl --player=spotify status 2> /dev/null)
 if [ "$player_status" = "Playing" ]; then
-    echo "$(playerctl metadata artist) - $(playerctl metadata title)"
+    echo "$(playerctl --player=spotify metadata artist) - $(playerctl --player=spotify metadata title)"
 elif [ "$player_status" = "Paused" ]; then
-    echo " $(playerctl metadata artist) - $(playerctl metadata title)"
+    echo " $(playerctl --player=spotify metadata artist) - $(playerctl --player=spotify metadata title)"
 else
     echo ""
 fi
